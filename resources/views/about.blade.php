47b3caf31a1a71c9c2f8a845d3ca992003d7eee5
ronyecz.peter <ronyecz.peter@everest.hu>
2021-08-18 09:01:03 +0200

    -- commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,10 +9,12 @@
             <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
             <div class="container">
                 <div class="banner_content text-center">
-                    <h2>About Me</h2>
-                    <div class="page_link">
-                        <a href="/">Home</a>
-                        <a href="/about">About Me</a>
+                    <div class="baner_box">
+                        <h2>About Me</h2>
+                        <div class="page_link">
+                            <a href="/">Home</a>
+                            <a href="/about">About Me</a>
+                        </div>
                     </div>
 
                 </div>
