3ec20e2f57984a27e3bde38bedecda53b12f5099
ronyecz.peter <ronyecz.peter@everest.hu>
2021-11-10 00:18:52 +0100

    -- commit

--- public/js/theme.js
+++ public/js/theme.js
@@ -33,42 +33,6 @@
 	}
 	parallaxEffect();
 
-
-	/*----------------------------------------------------*/
-    /*  Clients Slider
-    /*----------------------------------------------------*/
-    function clients_slider(){
-        if ( $('.clients_slider').length ){
-            $('.clients_slider').owlCarousel({
-                loop:true,
-                margin: 30,
-                items: 5,
-                nav: false,
-                autoplay: false,
-                smartSpeed: 1500,
-                dots:false,
-                responsiveClass: true,
-                responsive: {
-                    0: {
-                        items: 1,
-                    },
-                    400: {
-                        items: 2,
-                    },
-                    575: {
-                        items: 3,
-                    },
-                    768: {
-                        items: 4,
-                    },
-                    992: {
-                        items: 5,
-                    }
-                }
-            })
-        }
-    }
-    clients_slider();
 	/*----------------------------------------------------*/
     /*  MailChimp Slider
     /*-------------------
@@ -160,6 +124,9 @@
                         items: 1,
                     },
                     768: {
+                        items: 2,
+                    },
+                    1200: {
                         items: 3,
                     },
                 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -101,41 +101,38 @@
         <div class="container">
             <div class="main_title">
                 <h2>Testimonials</h2>
-                <p>Aliquam convallis turpis non ipsum maximus, vitae rhoncus nunc dapibus. Proin id massa eget eros finibus fringilla. Phasellus eu metus sed quam hendrerit placerat id id justo. </p>
             </div>
             <div class="testi_inner">
                 <div class="testi_slider owl-carousel">
                     <div class="item">
-                        <div class="testi_item">
-                            <p>Aliquam convallis turpis non ipsum maximus, vitae rhoncus nunc dapibus. Proin id massa eget eros finibus fringilla. Phasellus eu metus sed quam hendrerit placerat id id justo. </p>
-                            <h4>Lorem Ipsum </h4>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star-half-o"></i></a>
+                        <div class="testi_item d-flex align-items-center flex-nowrap flex-column justify-content-center">
+                            <p class="mh400">Before consulting with Dora, I had the usual difficulties when getting dressed, feeling: "I have nothing to wear, although my closet is full of clothes".<br/>
+                                However, having had a personal style and body analysis, followed by a wardrobe editing consultation, now the time for getting dressed is reduced dramatically. I was given guidance as to how I should re-organize my wardrobe, how to create sets and outfits easily. During this process I also learnt how to revive some pieces that I have not really used before.</p>
+                            <small><i>/Orsolya, Hungary/</i> </small>
                         </div>
                     </div>
                     <div class="item">
-                        <div class="testi_item">
-                            <p>Aliquam convallis turpis non ipsum maximus, vitae rhoncus nunc dapibus. Proin id massa eget eros finibus fringilla. Phasellus eu metus sed quam hendrerit placerat id id justo. </p>
-                            <h4>Lorem Ipsum </h4>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star-half-o"></i></a>
+                        <div class="testi_item d-flex align-items-center flex-nowrap flex-column justify-content-center">
+                            <p class="mh400"> had a color consultation with Dora, and I must say I never realized which are the colors that suited me the best and which ones are the no-goes for me. But learning about my basic, neutral and statement colors make all the difference! Knowing my best colors, reduces the time I spend in clothes shops as by recognizing them I can focus really on the ones that suit me well and simply ignore the rest.</p>
+                            <small><i>/Barbara - Texas, US/</i> </small>
                         </div>
                     </div>
                     <div class="item">
-                        <div class="testi_item">
-                            <p>Aliquam convallis turpis non ipsum maximus, vitae rhoncus nunc dapibus. Proin id massa eget eros finibus fringilla. Phasellus eu metus sed quam hendrerit placerat id id justo. </p>
-                            <h4>Lorem Ipsum </h4>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star"></i></a>
-                            <a href="#"><i class="fa fa-star-half-o"></i></a>
+                        <div class="testi_item d-flex align-items-center flex-nowrap flex-column justify-content-center">
+                            <p class="mh400">As a man I was skeptical about having the need of a wardrobe consultant, but now I do not only know what to wear but also how to wear my clothes to be stylish and up-to-date even without having the need to buy new clothes.</p>
+                            <small><i>/Peter, Hungary/</i> </small>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testi_item d-flex align-items-center flex-nowrap flex-column justify-content-center">
+                            <p class="mh400">It is wonderful to learn which colors suit me best not to mention the ability to shop efficiently. Instead of searching and looking around aimlessly trying to find something which at time I wasn’t even sure about and yet purchased! Now, I am more of a savvy shopper when I enter a store and know exactly how to sort through the items. Thanks to Dora, I don’t have to buy a whole new pricey wardrobe and can utilize apparel I already have by simply being knowledgeable how to mix and match to wear them.</p>
+                            <small><i>/Kimberly – Texas, US/</i> </small>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="testi_item d-flex align-items-center flex-nowrap flex-column justify-content-center">
+                            <p class="mh400">I have long wanted to consult somebody about what type of clothing items were really matching my figure. Dora made me feel comfortable right from the beginning through the whole process. I learned about things like silhouettes, cuts, and fabrics that are best suitable specifically for me. I feel like a whole new person with more confidence. I am very thankful to her professional insight and highly recommend anyone who felt the way I did, to reach out to Dora for a consultation.</p>
+                            <small><i>/Kristin – Texas, US/</i> </small>
                         </div>
                     </div>
                 </div>
