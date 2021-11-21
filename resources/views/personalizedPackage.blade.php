953bce18e705d765e43feea45b3ae5341f5d5f1c
ronyecz.peter <ronyecz.peter@everest.hu>
2021-11-22 00:01:05 +0100

    -- commit

---
+++ resources/views/personalizedPackage.blade.php
@@ -0,0 +1,86 @@
+@extends('layout')
+
+@section('title', 'Teomoda | Personalized Package')
+
+@section('content')
+    <section class="banner_area">
+        <div class="banner_inner personalized_package d-flex align-items-center">
+            <div class="container">
+                <div class="banner_content text-center">
+                    <h2 class="banner_h2">Personalized Package</h2>
+                    <div class="page_link">
+                        <a class="banner_a" href="/">Home</a>
+                        <a class="banner_a" href="/personalized-package ">Personalized Package</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================End Home Banner Area =================-->
+    <section class="contact_area p_80">
+        <div class="whole-wrap">
+            <div class="container">
+                <div class="main_title main_title_service text-center">
+                    <h2 class="main_title_pre"><span class="main_title_service_text_left">“</span>Create your own style. Let it be unique for yourself and yet identifiable to others.<span class="main_title_service_text_right">”</span></h2>
+                    <small class="pull-right"><i>(Anna Wintour)</i></small>
+                </div>
+                <div class="section-top-border">
+                    <h3 class="mb-30 title_color">Discovering best Colors and Style suiting you and improving your Wardrobe! </h3>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <blockquote class="generic-blockquote">
+                                <p>Sign up for a personalized color and style consultation for a color and a body shape analysis, and guidance with your wardrobe organization on how to make necessary adjustments, to not only fulfill your personal styling needs but also make you feel comfortable, confident, and vibrant.</p>
+                                <p>With a free 15-30 min. free online initial consultation, I give you an insight into the procedures and guide you on simple, yet critical preparations required. </p>
+                                <p>Once ready, we will then schedule the consultation session. </p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-top-border">
+                    <div class="row">
+                        <div class="col-md-10 mt-sm-30 typo-sec">
+                            <h3 class="mb-20 title_color">Knowing your body shape and personal style will:</h3>
+                            <div class="">
+                                <ul class="unordered-list">
+                                    <li>Boost your confidence level.</li>
+                                    <li>Make you appreciate your wardrobe with knowledge on sorting the pieces that are right for you.</li>
+                                    <li>Expand your knowledge on range of clothing items you can wear versus just keeping to a few safer ones.</li>
+                                    <li>Provide you with a more effective way to shop in both cost and time.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-top-border">
+                    <div class="row">
+                        <div class="col-md-10 mt-sm-30 typo-sec">
+                            <h3 class="mb-20 title_color">By the end of the consultation, you will know:</h3>
+                            <div class="">
+                                <ul class="unordered-list">
+                                    <li>Your best colors and how to wear them </li>
+                                    <li>What body shape you are</li>
+                                    <li>How to use accessories that can make your old clothes look stylish and even more attractive</li>
+                                    <li>What clothing silhouettes, lines, cuts, fabrics and prints best compliment your figure.</li>
+                                    <li>What staple items are essential to have in your “capsule” wardrobe.</li>
+                                    <li>How to combine items to best represent the style and image that make your feel your best.</li>
+                                    <li>What accessories to have and how to use them.</li>
+                                    <li>The importance of your facial features and its implications on your outfit and accessories.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-top-border">
+                    <div class="wel_item">
+                        <i class="lnr lnr-database"></i>
+                        <h4>$120</h4>
+                        <p><strong>Available: </strong> In-person or Online</p>
+                        <p><strong>Length: </strong> This service includes a 15-30 min. free online interview and preparation session (Zoom, Skype, Facetime) followed by a 120 min. in-person consultation.</p>
+                        <a id="personalized-package" href="/contact" class="btn btn-danger mt-3 position-relative">choose</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
