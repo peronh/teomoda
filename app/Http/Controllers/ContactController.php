513e8a32c424ef09d694f8cff6d33d2221fa69c9
ronyecz.peter <ronyecz.peter@everest.hu>
2023-12-03 23:49:41 +0100

    add recaptcha#9

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -40,16 +40,17 @@ class ContactController extends Controller
             'subject' => $request->get('subject')
         ];
 
-        $recaptcha = new ReCaptcha(env('GOOGLE_RECAPTCHA_SECRET'));
+        //TODO: uncomment this when you want to use recaptcha
+//        $recaptcha = new ReCaptcha(env('GOOGLE_RECAPTCHA_SECRET'));
 
 //        $response = $recaptcha->setExpectedHostname('teomoda.com')
 //            ->setExpectedAction('homepage')
 //            ->setScoreThreshold(0.5)
 //            ->verify($request->input('g-recaptcha-response'));
 
-        $response = $recaptcha->verify($request->input('g-recaptcha-response'));
+//        $response = $recaptcha->verify($request->input('g-recaptcha-response'));
 
-        if ($response->isSuccess()) {
+//        if ($response->isSuccess()) {
             Mail::send('emails.email',
                 $data,
                 function ($message) {
@@ -57,9 +58,9 @@ class ContactController extends Controller
                     $message->to('dora@teomoda.com', 'Dora Kery')
                         ->subject('Message from teomoda.com website');
                 });
-        } else {
-            return redirect()->back()->with('error', $response);
-        }
+//        } else {
+//            return redirect()->back()->with('error', $response);
+//        }
 
 
 
